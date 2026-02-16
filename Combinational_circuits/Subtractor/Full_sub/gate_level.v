a7c9577e751392776fcca50a49a8f470dbe6611d
ARJUN R <arjunravi2134@gmail.com>
2026-02-16 16:03:44 +0530

    Add full subtractor module in gate_level.v

---
+++ Combinational_circuits/Subtractor/Full_sub/gate_level.v
@@ -0,0 +1,19 @@
+module full_sub (
+    input  a,
+    input  b,
+    input  bin,
+    output diff,
+    output br
+);
+
+wire w1, w2, w3;
+
+xor (w1, a, b);
+xor (diff, w1, bin);
+
+
+and (w2,~ a, b);
+and (w3, ~w1, bin);
+or  (br, w2, w3);
+
+endmodule
