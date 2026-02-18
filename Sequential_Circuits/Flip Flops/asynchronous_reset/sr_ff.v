53e33567fdc71156350d6742d3d8a8bb48bb6510
ARJUN R <arjunravi2134@gmail.com>
2026-02-18 19:58:59 +0530

    Add SR flip-flop module with reset capability
    Implement SR flip-flop with asynchronous reset functionality.

---
+++ Sequential_Circuits/Flip Flops/asynchronous_reset/sr_ff.v
@@ -0,0 +1,15 @@
+module srff(input s,r,c,rt,output reg q);
+  always@ (posedge c or posedge rt)
+    begin
+      if(rt)
+        q<=1'b0;
+      else if(s&r)
+        q<=1'bx;
+      else if(s)
+        q<=1'b1;
+      else if(r)
+        q<=1'b0;
+  
+      
+    end
+endmodule
