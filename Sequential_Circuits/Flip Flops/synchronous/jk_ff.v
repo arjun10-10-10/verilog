966672ab57efa353083ed0c82fb39d8549f4e0ef
ARJUN R <arjunravi2134@gmail.com>
2026-02-18 20:01:19 +0530

    Add JK flip-flop module
    Implement JK flip-flop with reset functionality.

---
+++ Sequential_Circuits/Flip Flops/synchronous/jk_ff.v
@@ -0,0 +1,16 @@
+module jk(input j,k,clk,rst,output reg q );
+  always@(posedge clk )
+    begin
+      if(rst)
+        q<=1'b0;
+     
+      else if (j&k)
+        q<=~q;
+ else if(j)
+        q<=1'b1;
+      else if(k)
+        q<=1'b0;
+     
+      
+       end
+endmodule
