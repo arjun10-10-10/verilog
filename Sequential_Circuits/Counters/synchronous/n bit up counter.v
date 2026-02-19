c4d97e6392ab4d94bf89e385a2a5147e000d6a75
ARJUN R <arjunravi2134@gmail.com>
2026-02-19 12:54:33 +0530

    Change counter parameter from 3 to 8

---
+++ Sequential_Circuits/Counters/synchronous/n bit up counter.v
@@ -0,0 +1,12 @@
+module counter #(parameter n=8)(input clk,rst,en,output reg [n-1:0]q=0);
+
+  always@(posedge clk )
+    begin
+      if(rst)
+        q<=0;
+      else if(en)
+        q<=q + 1;
+      else
+        q<=q;
+    end
+endmodule
