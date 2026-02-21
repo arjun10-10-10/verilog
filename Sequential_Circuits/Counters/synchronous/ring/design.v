66fd5eaf9da39eb4f99ec8062d90f335f6e726e8
ARJUN R <arjunravi2134@gmail.com>
2026-02-21 14:22:12 +0530

    Create design.v

---
+++ Sequential_Circuits/Counters/synchronous/ring/design.v
@@ -0,0 +1,18 @@
+
+module ring #(parameter a=4)(input clk,rst,output [a-1:0]q=4'b0001);
+  
+  always@(posedge clk) begin
+    
+    if(rst)
+      
+      
+      q<=4'b0000;
+    
+    else
+      
+      q<={q[a-2:0],q[a-1]};
+    
+    
+  end
+     
+endmodule
