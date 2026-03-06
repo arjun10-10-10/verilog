dd37555e488f951b7dfc38237743e9d8d285e789
ARJUN R <arjunravi2134@gmail.com>
2026-03-06 21:13:19 +0530

    Create frq_div_4_50%duty.v

---
+++ Sequential_Circuits/Frequency Divider/design/frq_div_4_50%duty.v
@@ -0,0 +1,14 @@
+module frq_div_by_4(input clk,rst,output  fout);
+  
+  reg [1:0]count=0;
+  
+  always@(posedge clk)begin
+    
+    if(rst)
+    count<=0;
+    else 
+      count<=count+1;
+    end
+  
+  assign fout =(count==2'b01)?0:1;
+endmodulemodule
