4138a349a763951c99b25f635c2f4046b3f8ea3b
ARJUN R <arjunravi2134@gmail.com>
2026-03-29 09:25:00 +0530

    Create sync.v

---
+++ Sequential_Circuits/FIFO/ASYN_FIFO/sync.v
@@ -0,0 +1,20 @@
+module sync #(parameter pnt_size = 5)(
+    input sclk,
+    input srst_n,
+    input [pnt_size-1:0] din,
+    output reg [pnt_size-1:0] dout
+);
+
+    reg [pnt_size-1:0] flop1;
+
+    always @(posedge sclk or negedge srst_n) begin
+        if (!srst_n) begin
+            flop1 <= 0;
+            dout  <= 0;
+        end else begin
+            flop1 <= din;
+            dout  <= flop1;
+        end
+    end
+
+endmodule
