43d96357c094255635cbd2f3ef99121f8a2c9523
ARJUN R <arjunravi2134@gmail.com>
2026-03-30 10:35:07 +0530

    Update design.v

--- Sequential_Circuits/FIFO/SYNC_FIFO/design.v
+++ Sequential_Circuits/FIFO/SYNC_FIFO/design.v
@@ -1,27 +1,33 @@
-module fifo (
-    input clk, 
-    input rst, 
-    input wen, 
-    input ren, 
-    input din, 
-    output reg dout 
-   
+module fifo #(
+    parameter DATA_WIDTH = 4,
+    parameter DEPTH = 8,
+    parameter ADDR_WIDTH = $clog2(DEPTH)
+)(
+    input clk,
+    input rst_n,
+    input wen,
+    input ren,
+    input [DATA_WIDTH-1:0] din,
+    output reg [DATA_WIDTH-1:0] dout,
+    output full,
+    output empty
 );
-   wire full,empty;
-  reg [2:0] wptr=0, rptr=0;
-    reg [0:7] mem;
+
+    reg [ADDR_WIDTH-1:0] wptr = 0, rptr = 0;
+    reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
     integer i;
+    assign empty = (wptr == rptr);
+    assign full  = ((wptr + 1'b1) == rptr);
 
-  assign empty = (wptr == rptr)?1:0;
-  assign full  = ((wptr + 3'b001) == rptr)?1:0;
-
-    always @(posedge clk or negedge rst) begin
-        if (!rst) begin
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
             wptr <= 0;
             rptr <= 0;
             dout <= 0;
-            for (i = 0; i < 8; i = i + 1)
+
+           for (i = 0; i < DEPTH; i = i + 1)
                 mem[i] <= 0;
+
         end else begin
             if (wen && !full) begin
                 mem[wptr] <= din;
